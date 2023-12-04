248c3a98cd0333eb140188c1dd2e55da7af116ba
Raffaele Sena <raff367@gmail.com>
2023-12-03 19:11:11 -0800

    reformat

--- done/3/code3.go
+++ done/3/code3.go
@@ -16,64 +16,64 @@ func main() {
 
 	m, _ := matrix.FromSlice(cols, false, []byte(Join(lines, "")))
 
-        total := 0
+	total := 0
 
 	n := 0
 	part := byte(0)
-        partLoc := 0
+	partLoc := 0
 
-        gears := map[int][]int{}    // part coordinats: part values
+	gears := map[int][]int{} // part coordinats: part values
 
-        addnum := func() {
+	addnum := func() {
 		if n != 0 {
-                        if part != 0 {
-                            total += n
-			    fmt.Println(n, string(part))
+			if part != 0 {
+				total += n
+				fmt.Println(n, string(part))
 
-                            if part == '*' {
-                                gears[partLoc] = append(gears[partLoc], n)
-                            }
-                        }
+				if part == '*' {
+					gears[partLoc] = append(gears[partLoc], n)
+				}
+			}
 
 			n, part, partLoc = 0, 0, 0
 		}
-        }
+	}
 
 	for y := 0; y < m.Height(); y++ {
 		for x := 0; x < m.Width(); x++ {
 			c := m.Get(x, y)
 			if c >= '0' && c <= '9' {
-				n = n*10 + int(c - '0')
+				n = n*10 + int(c-'0')
 
 				adj := m.Adjacent(x, y, false)
 				for _, v := range adj {
 					if (v.Value < '0' || v.Value > '9') && v.Value != '.' {
 						part = v.Value
-                                                partLoc = v.X * 1000 + v.Y
-                                                break
+						partLoc = v.X*1000 + v.Y
+						break
 					}
 				}
 			} else {
-                            addnum()
+				addnum()
 			}
 		}
 
-                addnum()
+		addnum()
 	}
 
-        addnum()
+	addnum()
 
-        fmt.Println()
-        fmt.Println("total:", total)
+	fmt.Println()
+	fmt.Println("total:", total)
 
-        total = 0
+	total = 0
 
-        for _, v := range gears {
-            if len(v) == 2 {
-                r := v[0] * v[1]
-                total += r
-            }
-        }
+	for _, v := range gears {
+		if len(v) == 2 {
+			r := v[0] * v[1]
+			total += r
+		}
+	}
 
-        fmt.Println("total ratios:", total)
+	fmt.Println("total ratios:", total)
 }
