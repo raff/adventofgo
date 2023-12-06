b4422a5193c6185c698b1a0b6a8f8f6dff8fd442
Raffaele Sena <raff367@gmail.com>
2023-12-05 19:26:02 -0800

    Those should be been renamed, but got deleted :(

---
+++ 2023/1/code1.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/raff/advent2022/advlib"
+)
+
+func getnum(l string, first bool) (n int) {
+
+	for len(l) > 0 {
+		switch {
+		case l[0] == '1':
+			n = 1
+			l = l[1:]
+
+		case strings.HasPrefix(l, "one"):
+			n = 1
+			l = l[2:] // (e)ight
+
+		case l[0] == '2':
+			n = 2
+			l = l[1:]
+
+		case strings.HasPrefix(l, "two"):
+			n = 2
+			l = l[2:] // (o)ne
+
+		case l[0] == '3':
+			n = 3
+			l = l[1:]
+
+		case strings.HasPrefix(l, "three"):
+			n = 3
+			l = l[2:] // (e)ight
+
+		case l[0] == '4':
+			n = 4
+			l = l[1:]
+
+		case strings.HasPrefix(l, "four"):
+			n = 4
+			l = l[4:]
+
+		case l[0] == '5':
+			n = 5
+			l = l[1:]
+
+		case strings.HasPrefix(l, "five"):
+			n = 5
+			l = l[3:] // (e)ight
+
+		case l[0] == '6':
+			n = 6
+			l = l[1:]
+
+		case strings.HasPrefix(l, "six"):
+			n = 6
+			l = l[3:]
+
+		case l[0] == '7':
+			n = 7
+			l = l[1:]
+
+		case strings.HasPrefix(l, "seven"):
+			n = 7
+			l = l[4:] // (n)ine
+
+		case l[0] == '8':
+			n = 8
+			l = l[1:]
+
+		case strings.HasPrefix(l, "eight"):
+			n = 8
+			l = l[4:] // (t)wo or (t)hree
+
+		case l[0] == '9':
+			n = 9
+			l = l[1:]
+
+		case strings.HasPrefix(l, "nine"):
+			n = 9
+			l = l[3:] // (e)ight
+
+		default:
+			l = l[1:]
+			continue
+		}
+
+		//fmt.Printf("n: %v line: %q\n", n, l)
+
+		if first {
+			return n
+		}
+	}
+
+	return n
+}
+
+func main() {
+	r := advlib.NewReader()
+	lines, _ := r.Readlines()
+
+	total := 0
+
+	for _, line := range lines {
+		first := getnum(line, true)
+		last := getnum(line, false)
+
+		if first < 0 || last < 0 {
+			fmt.Println("invalid input:", line)
+			return
+		}
+
+		n := first*10 + last
+		fmt.Println(line, "-", n)
+		total += n
+	}
+
+	fmt.Println("total:", total)
+}
---
+++ 2023/2/code2.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+
+	. "github.com/raff/advent2022/advlib"
+)
+
+var (
+	max = map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+)
+
+func main() {
+	r := NewReader()
+	lines, _ := r.Readlines()
+
+	total := 0
+
+	//next_line:								; part 1
+	for _, line := range lines {
+		parts := SplitSep(line, ": ")
+
+		_, game := Split2(parts[0])
+		subs := SplitSep(parts[1], "; ")
+
+		min := map[string]int{}
+
+		for _, s := range subs {
+			for _, c := range SplitSep(s, ", ") {
+				n, color := Split2(c)
+				in := ParseInt(n)
+				/*						; part 1
+				if in > max[color] {
+					fmt.Println("skip game", game)
+					continue next_line
+				}
+				*/
+				if min[color] < in {
+					min[color] = in
+				}
+			}
+		}
+
+		//fmt.Println(game, min)					; part 1
+		//total += ParseInt(game)
+
+		p := 1
+		for _, v := range min {
+			p *= v
+		}
+
+		fmt.Println(game, p)
+		total += p
+	}
+
+	fmt.Println()
+	fmt.Println("total", total)
+}
